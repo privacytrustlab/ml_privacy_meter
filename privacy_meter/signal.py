c555fcf7115e491a42b3b785339d0556359fa500
Victor MASIAK <victor.masiak@gmail.com>
2022-03-23 19:28:18 +0800

    New Signal class + 4 childs: ModelOutput, ModelIntermediateOutput, ModelLoss, ModelGradient

--- privacy_meter/signal.py
+++ privacy_meter/signal.py
@@ -1,34 +1,212 @@
-from .model import Model
-from .dataset import Dataset
+from abc import ABC, abstractmethod
 
-def loss_signal_func(model: Model, dataset: Dataset,
-                     split_name,
-                     input_feature_name='<default_input>',
-                     output_feature_name='<default_output>',
-                     indices=None):
+from typing import List, Tuple
+
+from privacy_meter.dataset import Dataset
+from privacy_meter.model import Model
+
+
+class Signal(ABC):
     """
-    Function to return the loss values over the specified split of the
-    provided dataset using the provided model.
-    Args:
-        model: Model used for computing loss values
-        dataset: Dataset which will be used for providing input to the model
-        split_name: Name of the split
-        input_feature_name: Name of the input feature
-        output_feature_name: Name of output feature e.g. true labels
-        indices: Optional list of indices. If not specified, the entire subset is returned.
-    Returns:
-        loss_values: Sequence of loss values computed using the provided model and dataset
+    Abstract class, representing any type of signal that can be obtained from a Model and/or a Dataset.
     """
-    print(f"Getting loss values from {split_name} split of the dataset...")
 
-    loss_fn = tf.keras.losses.CategoricalCrossentropy(from_logits=True,
-                                                      reduction=tf.keras.losses.Reduction.NONE)
+    @abstractmethod
+    def __call__(self,
+                 models: List[Model],
+                 datasets: List[Dataset],
+                 model_to_split_mapping: List[Tuple[int, str, str, str]],
+                 extra: dict
+                 ):
+        """Built-in call method.
 
-    y_true = dataset.get_feature(split_name=split_name, feature_name=output_feature_name, indices=indices)
+        Args:
+            models: List of models that can be queried.
+            datasets: List of datasets that can be queried.
+            model_to_split_mapping: List of tuples, indicating how each model should query the dataset.
+                More specifically, for model #i:
+                model_to_split_mapping[i][0] contains the index of the dataset in the list,
+                model_to_split_mapping[i][1] contains the name of the split,
+                model_to_split_mapping[i][2] contains the name of the input feature,
+                model_to_split_mapping[i][3] contains the name of the output feature.
+                This can also be provided once and for all at the instantiation of InformationSource, through the
+                default_model_to_split_mapping argument.
+            extra: Dictionary containing any additional parameter that should be passed to the signal object.
 
-    x = dataset.get_feature(split_name=split_name, feature_name=input_feature_name, indices=indices)
-    y_pred = model.get_outputs(batch_samples=x)
+        Returns:
+            The signal value.
+        """
+        pass
 
-    loss_values = loss_fn(y_true=y_true, y_pred=y_pred)
 
-    return loss_values
+class ModelOutput(Signal):
+    """
+    Inherits of the Signal class, used to represent any type of signal that can be obtained from a Model and/or a Dataset.
+    This particular class is used to get the output of a model.
+    """
+
+    def __call__(self,
+                 models: List[Model],
+                 datasets: List[Dataset],
+                 model_to_split_mapping: List[Tuple[int, str, str, str]],
+                 extra: dict
+                 ):
+        """Built-in call method.
+
+        Args:
+            models: List of models that can be queried.
+            datasets: List of datasets that can be queried.
+            model_to_split_mapping: List of tuples, indicating how each model should query the dataset.
+                More specifically, for model #i:
+                model_to_split_mapping[i][0] contains the index of the dataset in the list,
+                model_to_split_mapping[i][1] contains the name of the split,
+                model_to_split_mapping[i][2] contains the name of the input feature,
+                model_to_split_mapping[i][3] contains the name of the output feature.
+                This can also be provided once and for all at the instantiation of InformationSource, through the
+                default_model_to_split_mapping argument.
+            extra: Dictionary containing any additional parameter that should be passed to the signal object.
+
+        Returns:
+            The signal value.
+        """
+
+        results = []
+        # Compute the signal for each model
+        for k, model in enumerate(models):
+            # Extract the features to be used
+            dataset_index, split_name, input_feature, output_feature = model_to_split_mapping[k]
+            x = datasets[dataset_index].get_feature(split_name, input_feature)
+            # Compute the signal
+            results.append(model.get_outputs(x))
+        return results
+
+
+class ModelIntermediateOutput(Signal):
+    """
+    Inherits of the Signal class, used to represent any type of signal that can be obtained from a Model and/or a Dataset.
+    This particular class is used to get the value of an intermediate layer of model.
+    """
+
+    def __call__(self,
+                 models: List[Model],
+                 datasets: List[Dataset],
+                 model_to_split_mapping: List[Tuple[int, str, str, str]],
+                 extra: dict
+                 ):
+        """Built-in call method.
+
+        Args:
+            models: List of models that can be queried.
+            datasets: List of datasets that can be queried.
+            model_to_split_mapping: List of tuples, indicating how each model should query the dataset.
+                More specifically, for model #i:
+                model_to_split_mapping[i][0] contains the index of the dataset in the list,
+                model_to_split_mapping[i][1] contains the name of the split,
+                model_to_split_mapping[i][2] contains the name of the input feature,
+                model_to_split_mapping[i][3] contains the name of the output feature.
+                This can also be provided once and for all at the instantiation of InformationSource, through the
+                default_model_to_split_mapping argument.
+            extra: Dictionary containing any additional parameter that should be passed to the signal object.
+
+        Returns:
+            The signal value.
+        """
+
+        if 'layers' not in list(extra):
+            raise TypeError('extra parameter "layers" is required')
+
+        results = []
+        # Compute the signal for each model
+        for k, model in enumerate(models):
+            # Extract the features to be used
+            dataset_index, split_name, input_feature, output_feature = model_to_split_mapping[k]
+            x = datasets[dataset_index].get_feature(split_name, input_feature)
+            # Compute the signal
+            results.append(model.get_intermediate_outputs(extra["layers"], x))
+        return results
+
+
+class ModelLoss(Signal):
+    """
+    Inherits of the Signal class, used to represent any type of signal that can be obtained from a Model and/or a Dataset.
+    This particular class is used to get the loss of a model.
+    """
+
+    def __call__(self,
+                 models: List[Model],
+                 datasets: List[Dataset],
+                 model_to_split_mapping: List[Tuple[int, str, str, str]],
+                 extra: dict
+                 ):
+        """Built-in call method.
+
+        Args:
+            models: List of models that can be queried.
+            datasets: List of datasets that can be queried.
+            model_to_split_mapping: List of tuples, indicating how each model should query the dataset.
+                More specifically, for model #i:
+                model_to_split_mapping[i][0] contains the index of the dataset in the list,
+                model_to_split_mapping[i][1] contains the name of the split,
+                model_to_split_mapping[i][2] contains the name of the input feature,
+                model_to_split_mapping[i][3] contains the name of the output feature.
+                This can also be provided once and for all at the instantiation of InformationSource, through the
+                default_model_to_split_mapping argument.
+            extra: Dictionary containing any additional parameter that should be passed to the signal object.
+
+        Returns:
+            The signal value.
+        """
+
+        results = []
+        # Compute the signal for each model
+        for k, model in enumerate(models):
+            # Extract the features to be used
+            dataset_index, split_name, input_feature, output_feature = model_to_split_mapping[k]
+            x = datasets[dataset_index].get_feature(split_name, input_feature)
+            y = datasets[dataset_index].get_feature(split_name, output_feature)
+            # Compute the signal
+            results.append(model.get_loss(x, y))
+        return results
+
+
+class ModelGradient(Signal):
+    """
+    Inherits of the Signal class, used to represent any type of signal that can be obtained from a Model and/or a Dataset.
+    This particular class is used to get the gradient of a model.
+    """
+
+    def __call__(self,
+                 models: List[Model],
+                 datasets: List[Dataset],
+                 model_to_split_mapping: List[Tuple[int, str, str, str]],
+                 extra: dict
+                 ):
+        """Built-in call method.
+
+        Args:
+            models: List of models that can be queried.
+            datasets: List of datasets that can be queried.
+            model_to_split_mapping: List of tuples, indicating how each model should query the dataset.
+                More specifically, for model #i:
+                model_to_split_mapping[i][0] contains the index of the dataset in the list,
+                model_to_split_mapping[i][1] contains the name of the split,
+                model_to_split_mapping[i][2] contains the name of the input feature,
+                model_to_split_mapping[i][3] contains the name of the output feature.
+                This can also be provided once and for all at the instantiation of InformationSource, through the
+                default_model_to_split_mapping argument.
+            extra: Dictionary containing any additional parameter that should be passed to the signal object.
+
+        Returns:
+            The signal value.
+        """
+
+        results = []
+        # Compute the signal for each model
+        for k, model in enumerate(models):
+            # Extract the features to be used
+            dataset_index, split_name, input_feature, output_feature = model_to_split_mapping[k]
+            x = datasets[dataset_index].get_feature(split_name, input_feature)
+            y = datasets[dataset_index].get_feature(split_name, output_feature)
+            # Compute the signal
+            results.append(model.get_grad(x, y))
+        return results
