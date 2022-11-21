update the pytorch model, gradient norm function, fix minor bug, allow user defined saving

privacy_meter/hypothesis_test.py
@@ -91,8 +91,10 @@ def logit_rescale_threshold_func(
     Returns:
         threshold: alpha quantile of the provided distribution.
     """
+
+    distribution = distribution+0.000001 # avoid nan
     distribution = np.log(np.divide(np.exp(- distribution), (1 - np.exp(- distribution))))
-    len_dist = len(distribution)
+  
     if len(distribution.shape)>1:
         parameters = np.array([norm.fit(distribution[i]) for i in range(distribution.shape[0])])
         num_threshold = alpha.shape[0]
@@ -101,9 +103,9 @@ def logit_rescale_threshold_func(
         scale = parameters[:,1].reshape(-1,1).repeat(num_threshold,1)
         alpha = np.array(alpha).reshape(-1,1).repeat(num_points,1)
         threshold = norm.ppf(1-np.array(alpha),loc=loc.T,scale=scale.T)
-        np.place(threshold,threshold==-np.inf, distribution.min()-0.01)
-        np.place(threshold,threshold==np.inf,distribution.max()+0.01)
     else:
+        print('none')
+        print(np.sum(distribution==-np.inf))
         loc,scale = norm.fit(distribution)
         threshold = norm.ppf(1 - np.array(alpha), loc=loc, scale=scale)
     

privacy_meter/metric.py
@@ -64,8 +64,11 @@ class Metric(ABC):
         Returns:
             Signals computed using the specified information source and mapping object.
         """
-        signal_filepath = f'{self.logs_dirname}/{type(self).__name__}_{signal_source.value}'
-
+        if self.logs_dirname is not None:
+            signal_filepath = f'{self.logs_dirname}/{type(self).__name__}_{signal_source.value}'
+        else:
+            signal_filepath = None
+            
         if signal_source == SignalSourceEnum.TARGET_MEMBER:
             info_source_obj = self.target_info_source
             mapping_obj = self.target_model_to_train_split_mapping
@@ -92,8 +95,9 @@ class Metric(ABC):
                 signals.append(
                     info_source_obj.get_signal(signal, mapping_obj)
                 )
-            np.savez(signal_filepath, signals)
-
+            if signal_filepath is not None:
+                np.savez(signal_filepath, signals)
+        
         return signals
 
     def _load_or_compute_group_membership(
@@ -779,5 +783,6 @@ class GroupPopulationMetric(Metric):
                                         predicted_labels=predictions,
                                         true_labels=true_labels,
                                         predictions_proba=None,
-                                        signal_values=signal_values)
+                                        signal_values=signal_values,
+                                        threshold= reference_thresholds)
         return [metric_result]

privacy_meter/metric_result.py
@@ -83,6 +83,7 @@ class CombinedMetricResult:
             predictions_proba=None,
             signal_values=None,
             threshold: float = None
+            
     ):
         """
         Constructor.
@@ -102,7 +103,7 @@ class CombinedMetricResult:
         self.predictions_proba = predictions_proba
         self.signal_values = signal_values
         self.threshold = threshold
-
+        
         self.accuracy = np.mean(predicted_labels == true_labels,axis=1)
         self.tn = np.sum(true_labels==0) - np.sum(predicted_labels[:,true_labels==0],axis=1)
         self.tp = np.sum(predicted_labels[:,true_labels==1],axis=1)